c99fbccc694d4b1f24caf5e3a78f5be224302b77
Daniel Bittman <danielbittman1@gmail.com>
2025-03-15 13:35:00 -0700

    Twizzler patches.

--- src/backends/twizzler.rs
+++ src/backends/twizzler.rs
@@ -2,6 +2,8 @@
 use crate::Error;
 use core::{mem::MaybeUninit, num::NonZeroU32};
 
+pub use crate::util::{inner_u32, inner_u64};
+
 #[inline]
 pub fn fill_inner(mut dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
     let res = twizzler_rt_abi::random::twz_rt_get_random(
